7438e9f20d57bf60c90d9a095362817b86d4a67d
Peter He <yueyang.he@student.education.wa.edu.au>
2025-02-28 18:27:51 +0800

    Added core class

--- main.py
+++ main.py
@@ -4,27 +4,12 @@ from Game_class import *
 from Log import *
 import Global_options
 import sys
+from core import *
 pygame.init()
 Log.initialise()
 Log.log('Pygame initialised skibidi')
 screen = pygame.display.set_mode((Global_options.SCREENW, Global_options.SCREENH))
-clock = pygame.time.Clock() #frames per tick
 running = True
 game = Game(screen)
 Log.log('created game instance ')
-game.LoadAssets()
-while running:
-    start_time = time.time()
-    dt = clock.tick(Global_options.FPS)/1000
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            running = False
-    game.Update(dt)
-    game.Render()
-    end_time = time.time()
-    diff = end_time-start_time
-    Log.log(f'skbidi toilet loves {dt}')
-    #print(1/diff)
-    pygame.display.flip()
-game.Onquit()
+Core.Start(game)
