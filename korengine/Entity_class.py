0a440389e4209fc22ba2b51cde7393ecf655dee0
Paolo <p.ferri1986@gmail.com>
2025-03-07 18:59:06 +0800

    entity

--- korengine/Entity_class.py
+++ korengine/Entity_class.py
@@ -1,4 +1,12 @@
 #This class represents and entity in the game engine
+from Log import *
 class Entity: 
     def __init__(self):
+        pass
+
+    def Update(self, DeltaTime):
+        Log.log("Updating entity")
+        pass
+
+    def Render(self):
         pass
--- korengine/Game_class.py
+++ korengine/Game_class.py
@@ -1,10 +1,25 @@
+from typing import List
 from Log import *
+from Entity_class import Entity
+
 class Game:
     def __init__(self, screen):
         self.screen = screen
         Log.log('game is initiated')
+        self.allEntities: List[Entity] = []
+
+        test = Entity()
+        self.AddEntity(test)
+
+
+    def AddEntity(self, ent):
+        self.allEntities.append(ent)
     def Update(self, Deltatime):
         Log.log('UPDATE')
+        
+        for e in self.allEntities:
+            e.Update(Deltatime)
+
         pass
     def Render(self):
         Log.log('RENDER')
