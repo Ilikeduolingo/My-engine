46155599a9d515678895a892e18e746906cf5dd9
Paolo <p.ferri1986@gmail.com>
2025-03-07 18:52:26 +0800

    clean up

--- korengine/core.py
+++ korengine/core.py
@@ -7,7 +7,7 @@ class Core:
     game_instace = None
     @staticmethod
     def Start(game:Game):
-        Log.log('Pygame initialised skibidi')
+        Log.log('Pygame initialised')
 
         game_instsance = game
         game_instsance.LoadAssets()
@@ -26,7 +26,6 @@ class Core:
             game.Render()
             end_time = time.time()
             diff = end_time-start_time
-            Log.log(f'skbidi toilet loves {dt}')
             #print(1/diff)
             pygame.display.flip()
         game.Onquit()
